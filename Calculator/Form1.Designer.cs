418ab289ee8fc61023c69d6ba9ffb0d4ed97f865
Jamie Sun <53037883+jamie-sun@users.noreply.github.com>
2019-07-18 21:05:29 +1200

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonClearEntry = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonPosNeg = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textMain = new System.Windows.Forms.Label();
+            this.textEquation = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClearEntry
+            // 
+            this.buttonClearEntry.FlatAppearance.BorderSize = 0;
+            this.buttonClearEntry.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonClearEntry, "buttonClearEntry");
+            this.buttonClearEntry.ForeColor = System.Drawing.Color.Black;
+            this.buttonClearEntry.Name = "buttonClearEntry";
+            this.buttonClearEntry.TabStop = false;
+            this.buttonClearEntry.UseVisualStyleBackColor = true;
+            this.buttonClearEntry.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.FlatAppearance.BorderSize = 0;
+            this.buttonClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonClear, "buttonClear");
+            this.buttonClear.ForeColor = System.Drawing.Color.Black;
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.TabStop = false;
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.FlatAppearance.BorderSize = 0;
+            this.buttonDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonDelete, "buttonDelete");
+            this.buttonDelete.ForeColor = System.Drawing.Color.Black;
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.TabStop = false;
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.FlatAppearance.BorderSize = 0;
+            this.buttonDiv.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonDiv, "buttonDiv");
+            this.buttonDiv.ForeColor = System.Drawing.Color.Black;
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.TabStop = false;
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.Button_OperatorClick);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.FlatAppearance.BorderSize = 0;
+            this.buttonMul.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonMul, "buttonMul");
+            this.buttonMul.ForeColor = System.Drawing.Color.Black;
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.TabStop = false;
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.Button_OperatorClick);
+            // 
+            // button9
+            // 
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Name = "button9";
+            this.button9.TabStop = false;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Name = "button8";
+            this.button8.TabStop = false;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Name = "button7";
+            this.button7.TabStop = false;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.FlatAppearance.BorderSize = 0;
+            this.buttonSub.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonSub, "buttonSub");
+            this.buttonSub.ForeColor = System.Drawing.Color.Black;
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.TabStop = false;
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.Button_OperatorClick);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Name = "button6";
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Name = "button5";
+            this.button5.TabStop = false;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Name = "button4";
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.FlatAppearance.BorderSize = 0;
+            this.buttonAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonAdd, "buttonAdd");
+            this.buttonAdd.ForeColor = System.Drawing.Color.Black;
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.TabStop = false;
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.Button_OperatorClick);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Name = "button3";
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Name = "button2";
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Name = "button1";
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonEqual
+            // 
+            this.buttonEqual.FlatAppearance.BorderSize = 0;
+            this.buttonEqual.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonEqual, "buttonEqual");
+            this.buttonEqual.ForeColor = System.Drawing.Color.Black;
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.TabStop = false;
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            this.buttonEqual.Click += new System.EventHandler(this.Button_EqualsClick);
+            // 
+            // buttonDot
+            // 
+            this.buttonDot.FlatAppearance.BorderSize = 0;
+            this.buttonDot.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonDot, "buttonDot");
+            this.buttonDot.ForeColor = System.Drawing.Color.Black;
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.TabStop = false;
+            this.buttonDot.UseVisualStyleBackColor = true;
+            this.buttonDot.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button0
+            // 
+            this.button0.FlatAppearance.BorderSize = 0;
+            this.button0.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.ForeColor = System.Drawing.Color.Black;
+            this.button0.Name = "button0";
+            this.button0.TabStop = false;
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonPosNeg
+            // 
+            this.buttonPosNeg.FlatAppearance.BorderSize = 0;
+            this.buttonPosNeg.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            resources.ApplyResources(this.buttonPosNeg, "buttonPosNeg");
+            this.buttonPosNeg.ForeColor = System.Drawing.Color.Black;
+            this.buttonPosNeg.Name = "buttonPosNeg";
+            this.buttonPosNeg.TabStop = false;
+            this.buttonPosNeg.UseVisualStyleBackColor = true;
+            this.buttonPosNeg.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.buttonClearEntry);
+            this.panel1.Controls.Add(this.buttonEqual);
+            this.panel1.Controls.Add(this.buttonClear);
+            this.panel1.Controls.Add(this.buttonDot);
+            this.panel1.Controls.Add(this.buttonDelete);
+            this.panel1.Controls.Add(this.button0);
+            this.panel1.Controls.Add(this.buttonDiv);
+            this.panel1.Controls.Add(this.buttonPosNeg);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.buttonAdd);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.buttonMul);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.buttonSub);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.Controls.Add(this.textEquation);
+            this.panel2.Controls.Add(this.textMain);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // textMain
+            // 
+            resources.ApplyResources(this.textMain, "textMain");
+            this.textMain.ForeColor = System.Drawing.Color.Black;
+            this.textMain.Name = "textMain";
+            // 
+            // textEquation
+            // 
+            resources.ApplyResources(this.textEquation, "textEquation");
+            this.textEquation.ForeColor = System.Drawing.Color.Black;
+            this.textEquation.Name = "textEquation";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonClearEntry;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonEqual;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonPosNeg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label textMain;
+        private System.Windows.Forms.Label textEquation;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        String operation = ""; //current state of math operation pressed
+        Double value = 0; //stores values with decimal places
+        Boolean resetdisplay = false;
+        Boolean operationPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //adding of button presses onto main screen
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+
+            switch (b.Name)
+            {
+                case "buttonClearEntry":
+                    textMain.Text = "0";
+                    break;
+                case "buttonClear":
+                    textMain.Text = "0";
+                    textEquation.Text = "";
+                    operation = "";
+                    break;
+                case "buttonDelete":
+                    if (textMain.Text.Length != 1)
+                    {
+                        textMain.Text = textMain.Text.Substring(0, textMain.Text.Length - 1);
+                    }
+                    else
+                    {
+                        textMain.Text = "0";
+                    }
+                    break;
+                case "buttonPosNeg":
+                    if (textMain.Text != "0")
+                    {
+                        if (textMain.Text.Substring(0, 1) == "-")
+                        {
+                            textMain.Text = textMain.Text.Substring(1, textMain.Text.Length - 1);
+                        }
+                        else
+                        {
+                            textMain.Text = "-" + textMain.Text;
+                        }
+                    }
+                    break;
+                case "buttonDot":
+                    if (!operationPressed)
+                    {
+                        if (!textMain.Text.Contains("."))
+                        {
+                            textMain.Text += ".";
+                        }
+                    }
+                    else
+                    {
+                        textMain.Text = "0.";
+                        resetdisplay = false;
+                    }
+                    break;
+                default:
+                    //normal number button is entered into screen on default
+                    if ((textMain.Text == "0") || (resetdisplay)) 
+                    {
+                        textMain.Text = "";
+                        resetdisplay = false;
+                    }
+                    operationPressed = false;
+                    textMain.Text += b.Text;
+                    break;
+            }
+
+        }
+
+        //main screen values and operations pressed 
+        //are placed onto secondary screen
+        private void Button_OperatorClick(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            resetdisplay = true;
+
+            if (!operationPressed)
+            {
+                if (operation == "") //first part of equation before operations pressed
+                {
+                    operation = b.Text;
+                    value = Convert.ToDouble(textMain.Text);
+                    textEquation.Text += textMain.Text + operation;
+                }
+
+                else //second part of equation after operations pressed
+                {
+                    textEquation.Text += textMain.Text + b.Text;
+                    solve();
+                    operation = b.Text;
+                }
+                operationPressed = true;
+            }
+        }
+
+        //function to solve math equations
+        private void solve()
+        {
+            switch (operation)
+            {
+                case "÷":
+                    value = value / Convert.ToDouble(textMain.Text);
+                    textMain.Text = value.ToString();
+                    break;
+                case "×":
+                    value = value * Convert.ToDouble(textMain.Text);
+                    textMain.Text = value.ToString();
+                    break;
+                case "-":
+                    value = value - Convert.ToDouble(textMain.Text);
+                    textMain.Text = value.ToString();
+                    break;
+                case "+":
+                    value = value + Convert.ToDouble(textMain.Text);
+                    textMain.Text = value.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Button_EqualsClick(object sender, EventArgs e)
+        {
+            solve();
+            resetdisplay = true;
+            operationPressed = false;
+            value = Convert.ToDouble(textMain.Text);
+            value = 0;
+            operation = "";
+            textEquation.Text = "";
+        }
+    }
+}
